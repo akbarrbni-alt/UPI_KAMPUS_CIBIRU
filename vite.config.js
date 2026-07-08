22bcb60c261ba6dcf7c9b60732626ad19eaa5dfa
akbarrbni-alt <akbarrbni@upi.edu>
2026-07-08 20:00:28 +0700

    Fix Vercel Vite deployment

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,6 @@ import { defineConfig, transformWithEsbuild } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  server: {
-    https: {
-      key: "./keys/localhost-key.pem",
-      cert: "./keys/localhost-cert.pem",
-    },
-    host: true,
-    port: 3000,
-  },
   plugins: [
     {
       name: "treat-js-files-as-jsx",
@@ -24,9 +16,7 @@ export default defineConfig({
     },
     react(),
   ],
-
   optimizeDeps: {
-    force: true,
     esbuildOptions: {
       loader: {
         ".js": "jsx",
